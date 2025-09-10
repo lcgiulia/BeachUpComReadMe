5c069711af5393c018e9a7da310da5f3f72a702a
Felipe Ferrari Da Silva <felipeferrari2722@gmail.com>
2025-09-10 20:46:52 -0300

    Backend

---
+++ BackEnd/beachup-app/src/main/java/com/beachup/beachup_app/BeachupAppApplication.java
@@ -0,0 +1,13 @@
+package com.beachup.beachup_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachupAppApplication.class, args);
+	}
+
+}
